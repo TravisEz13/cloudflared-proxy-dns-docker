fedef0c3db2f33abf2316fcc1f9db5cfc87db2d0
Travis Plunk <travis.plunk@microsoft.com>
2018-04-08 16:39:37 -0700

    move files to root  - rebuild

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ DOCKERFILE_PATH="$SCRIPTFOLDER/docker/Dockerfile"
 IMAGE_NAME="cloudflared-proxy-dns-docker-local"
 export DOCKERFILE_PATH
 export IMAGE_NAME
-"$SCRIPTFOLDER/docker/hooks/build"
+pushd $SCRIPTFOLDER
+"$SCRIPTFOLDER/hooks/build"
+popd
